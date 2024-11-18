aea3e37dc8fe9baa0221a1b257ee2f4f12215dee
manhnv <manhnv4997@gmail.com>
2024-11-18 21:36:51 +0700

    Optimize: content

--- assets/js/pages/goi-cuoc.js
+++ assets/js/pages/goi-cuoc.js
@@ -1,16 +1,30 @@
 $(document).ready(function () {
     handleTab();
+    checkParam();
 });
+
+function checkParam() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const slug = urlParams.get('slug'); // Get the value of the "slug" parameter
+    $('.services__item').removeClass('--active');
+    $('.list-package__content--main-content').removeClass('--active');
+    if (slug) {
+        $(`[target-data="${slug}"]`).addClass('--active');
+        $(`#${slug}`).addClass('--active');
+    } else {
+        $($('.services__item')[0]).addClass('--active');
+        $($('.list-package__content--main-content')).addClass('--active');
+    }
+}
 
 
 function handleTab() {
+
     $('.services__item').on('click', function () {
         const dataTarget = $(this).attr('target-data');
 
         $('.services__item').removeClass('--active');
         $(this).addClass('--active');
-
-        console.log(dataTarget, 'dataTarget');
 
         $('.list-package__content--main-content').removeClass('--active');
 
